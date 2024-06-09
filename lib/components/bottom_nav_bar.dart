18a392820dec8fd3591524585bf6ad74dfc3f925
anthef <anthonyef09@gmail.com>
2024-06-09 23:07:32 +0700

    commit again

--- lib/components/bottom_nav_bar.dart
+++ lib/components/bottom_nav_bar.dart
@@ -1,6 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
-class BottomNavBar extends StatelessWidget{
+class BottomNavBar extends StatelessWidget {
+  void Function(int)? onTabChange;
+  BottomNavBar({super.key, required this.onTabChange});
 
-  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: GNav(
+        padding: EdgeInsets.symmetric(vertical: 20),
+        color: Colors.grey[400],
+        activeColor: Colors.grey.shade700,
+        tabActiveBorder: Border.all(color: Colors.white),
+        tabBackgroundColor: Colors.grey.shade100,
+        mainAxisAlignment: MainAxisAlignment.center,
+        tabBorderRadius: 16,
+        onTabChange: (value) => onTabChange!(value),
+        tabs: const [
+          GButton(
+            icon: Icons.home,
+            text: 'Shop',
+          ),
+          GButton(
+            icon: Icons.shopping_basket_rounded,
+            text: 'Cart',
+          )
+        ],
+      ),
+    );
+  }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:sneaker_shop/components/bottom_nav_bar.dart';
+import 'package:sneaker_shop/pages/cart_page.dart';
+import 'package:sneaker_shop/pages/shop_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,10 +11,69 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _pages = [
+    const ShopPage(),
+    const CartPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      bottomNavigationBar: BottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Builder(builder: (context) {
+          return IconButton(
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+          );
+        }),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[900],
+        child: Column(
+          children: [
+            DrawerHeader(
+              child: Image.asset(
+                'lib/images/nike.png', 
+                color: Colors.white
+                ),
+              ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Divider(color: Colors.grey[800],
+              ),
+            ),
+          const Padding(
+            padding: EdgeInsets.only(left:25.0),
+            child: ListTile(
+              leading : Icon(Icons.home, color: Colors.white,),
+              title : Text(
+                'Home',
+                style: TextStyle(color:Colors.white),
+              ),
+            ),
+          )
+        ],)
+      ),
+      body: _pages[_selectedIndex],
     );
   }
 }
